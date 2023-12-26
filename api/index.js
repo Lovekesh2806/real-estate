0f874b081023400a408ab7af7c3182e64c4f7031
CHELLINKI LOKESH <116074049+Lovekesh2806@users.noreply.github.com>
2023-12-26 20:04:27 +0530

    create a middleware and a function to handle possible error

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from './routes/user.route'
+import userRouter from './routes/user.route.js';
+// import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -9,13 +10,18 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log('Connected to MongoDB!');
     }).catch((err) => {
         console.log(err);
-    });
+    }
+);
 
 const app = express();
 
+// app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
-})
+});
 
 
-app.use("/api/user",userRouter);
+
+app.use('/api/user',userRouter);
+// app.use('/api/routes/auth.route.js',authRouter);
