c8e7dc5d692dab23c41b4e91379c2b07693252cd
CHELLINKI LOKESH <116074049+Lovekesh2806@users.noreply.github.com>
2023-12-26 21:21:02 +0530

    complete sign up page ui

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
-// import authRouter from './routes/auth.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
@@ -24,4 +24,14 @@ app.listen(3000, () => {
 
 
 app.use('/api/user',userRouter);
-// app.use('/api/routes/auth.route.js',authRouter);
+app.use('/api/auth',authRouter);
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
